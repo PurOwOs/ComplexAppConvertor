8b4fdefcd7793fd2ddec77fe4b2c2d0f628a23bb
PurOwO <vlkgamingcz@gmail.com>
2025-01-29 16:01:49 +0100

    A eye pleaseing expirience

--- ComplexAppConvertor/Program.cs
+++ ComplexAppConvertor/Program.cs
@@ -8,11 +8,13 @@ class Program
         Console.WriteLine("Complex Number Calculator" +
                           "\nBy: Zdeněk Dudek" +
                           "\nDiscord: purowo" +
-                          "\nTelegram: username:@PurOwO link:https://t.me/PurOwO");
+                          "\nTelegram: username:@PurOwO link:https://t.me/PurOwO" +
+                          "\n");
         Console.ForegroundColor = ConsoleColor.DarkMagenta;
         double x = 0, y = 0, abs, radians, degrees;
         Console.WriteLine("tvar z = (x+jy)" +
-                          "\nVýstupní tvar = Goniometrický");
+                          "\nVýstupní tvar = Goniometrický" +
+                          "\n");
 
         Console.Write("x=");
         x = double.Parse(Console.ReadLine());
@@ -30,7 +32,8 @@ class Program
             degrees += 360;
         }
         Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.WriteLine($"Goniometrický Tvar komplexního čísla je:" +
+        Console.WriteLine($"\n" +
+                          $"Goniometrický Tvar komplexního čísla je:" +
                           $"\nz = {abs:F2} (cos {degrees:F5}° + j sin {degrees:F5}°)");
         Console.WriteLine();
         Console.WriteLine("Děkuji za použití. Stiskněte klávesu po ukončení");
